272642e36e38418e0c10ec1cede555647b4ff367
Naveen Rawat <naveen.rawat@sprint.com>
2020-11-29 11:41:11 +0530

    fonts added

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
 import ProductReducer from "./store/reducers/products.reducer";
 import ShopNavigator from "./navigation/ShopNavigator";
+
+const fetchFonts = (_) => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 const rootReducer = combineReducers({
   products: ProductReducer,
@@ -12,6 +23,18 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
