c35f82302663f1342590dfa83ea0af64a69cd073
Naveen Rawat <naveen.rawat@sprint.com>
2020-11-29 18:36:59 +0530

    orders action reducers added, side drawer added with products and orders navigation

---
+++ modals/orders.modal.js
@@ -0,0 +1,8 @@
+export default class Order {
+  constructor(id, items, totalAmount, data) {
+    this.id = id;
+    this.items = items;
+    this.totalAmount = totalAmount;
+    this.data = data;
+  }
+}
